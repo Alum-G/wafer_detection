9e846ea58b324d229d465029273c904ae00f93e2
Chinonso Alumona <alumonacg@gmail.com>
2022-10-23 09:53:42 +0100

    Config, pipeline and requiremts update

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export PYTHONPATH=/opt/homebrew/Caskroom/miniforge/base/lib/python3.9/site-packages
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
-    packages=find_packages(),
+    packages=find_packages(), #find package containing __init__.py
     version='0.1.0',
     description='Predicting wafer condition',
     author='Nonso Alumona',
